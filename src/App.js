53c7390f73f439f1a947ef04ec576f371d85de71
Ovidiu Fosalau <ovidiu.fosalau@gmail.com>
2020-01-14 10:46:34 +0200

    added particles.js for nice effects

--- src/App.js
+++ src/App.js
@@ -4,16 +4,41 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
 import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
 
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 50,
+      density: {
+        enable: true,
+        value_area: 500
+      }
+    },
+    color: {
+      value: '#ffffff'
+    },
+    line_linked: {
+      shadow: {
+        enable: true,
+        color: "#3CA9D1",
+        blur: 9      }
+    }
+  }
+};
 
 function App() {
   return (
     <div className="App">
+      <Particles className='particles'
+        params={particlesOptions} />
       <Navigation />
       <Logo />
       <Rank />
-        <ImageLinkForm />
-        {/*<FaceRecognition />*/}
+      <ImageLinkForm />
+      {/*<FaceRecognition />*/}
+
     </div>
   );
 };
