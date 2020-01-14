8696ae8bb22876d05bd8c6cf613259368a41613e
Ovidiu Fosalau <ovidiu.fosalau@gmail.com>
2020-01-14 10:27:17 +0200

    improved logo display, added Rank component and custom google font

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -6,8 +6,8 @@ import face_image from './face-detect.png';
 const Logo = () => {
     return (
         <div className="ma4 mt0">
-            <Tilt className="Tilt br2 shadow-2" options={{ max: 55 }} style={{ height: 150, width: 150 }} >
-                <div className='Tilt-inner pa3'>
+            <Tilt className="Tilt br2 shadow-2" options={{ max: 55 }} style={{ height: 125, width: 125 }} >
+                <div className='Tilt-inner pa2'>
                     <img style={{paddingTop: '5px'}} alt='logo' src={face_image}/>
                 </div>
             </Tilt>
