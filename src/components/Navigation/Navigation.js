a6a0fb535256cfabb38a9b4a871fa1c908ef921f
Ovidiu Fosalau <ovidiu.fosalau@gmail.com>
2020-01-14 10:18:18 +0200

    added Logo and ImageLinkForm components; Logo has tilt, ImageLinkForm has carbon fiber background

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Navigation = () => {
     return (
         <nav style={{ display: 'flex', justifyContent: 'flex-end', paddingRight: '20px' }}>
-            <p className='f3 link dim black underlined pa3 pointer'>Sign Out</p>
+            <p className='f3 link dim black underlined pa3 pointer' style={{color: 'white'}}>Sign Out</p>
         </nav>
     );
 };
